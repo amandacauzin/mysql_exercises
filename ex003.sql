220ffe9d02c4bad01ecae343d2aaec250c0e66b7
Amanda Cauzin <cauzinamanda@gmail.com>
2026-06-22 19:41:33 -0300

    create ex003

---
+++ ex003.sql
@@ -0,0 +1,6 @@
+insert into pessoas values
+(default,'Adalgiza', '1393-11-02', 'F', '63.2', '1.76', default),
+(default,'Amanda', '1998-01-15', 'F', '60.2', '1.62', default),
+(default,'Valentina', '2006-05-18', 'F', '71.2', '1.74', default);
+
+select * from pessoas;
