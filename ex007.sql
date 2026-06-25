bf512b9a9db9bd928242df754ccd5f0a09bf6172
Amanda Cauzin <cauzinamanda@gmail.com>
2026-06-25 09:42:38 -0300

    data export

---
+++ ex007.sql
@@ -0,0 +1,10 @@
+use cadastro;
+select * from cursos;
+
+desc gafanhotos;
+
+Para apagar o banco de dados;
+drop database cadastro;
+
+server: data export -> exporta banco de dados
+server: data import -> importa banco de dados
